64f1c413dba43e366f5996af3cc44e746a0feb38
Brandon Bahret <brandonbahret@gmail.com>
2017-04-01 20:25:40 -0400

    Refactored code

--- app/src/main/java/com/example/brandon/habitlogger/RecyclerViewAdapters/GroupDecoration.java
+++ app/src/main/java/com/example/brandon/habitlogger/RecyclerViewAdapters/GroupDecoration.java
@@ -8,7 +8,6 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.support.annotation.DimenRes;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextPaint;
 import android.view.View;
@@ -20,67 +19,87 @@ import static android.support.v4.content.ContextCompat.getColor;
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class GroupDecoration extends RecyclerView.ItemDecoration {
 
-    private final Callback callback;
-    private final TextPaint textPaint;
-    private final Paint linePaint;
-    private final int topGap;
-    private final int bottomGap;
-    private final int lineInset;
-    private final float lineWidth;
-    private final int textVerticalOffset;
-    private Paint.FontMetrics fontMetrics;
+    //region (Member attributes)
+    private int mTopGap;
+    private int mBottomGap;
+    private int mTextVerticalOffset;
+    private int mTextSize;
 
-    private Context mContext;
+    private int mTextColor;
+    private int mBackdropColor;
 
+    private TextPaint mTextPaint;
+    private Paint.FontMetrics mFontMetrics;
+    //endregion
+
+    //region Code responsible for providing an interface
+    private final Callback mCallback;
+    private Paint mBackdropPaint;
+    private float mBackdropHeight;
+
+    public interface Callback {
+        long getGroupId(int position);
+
+        @NonNull
+        String getGroupFirstLine(int position);
+    }
+    //endregion -- end --
+
+    //region Constructor {}
     public GroupDecoration(Context context, @DimenRes int textSizeRes, Callback callback) {
-        super();
-        this.callback = callback;
-        final Resources res = context.getResources();
+        mCallback = callback;
 
-        mContext = context;
+        fetchDimensions(context, textSizeRes);
 
-        int textSize = res.getDimensionPixelSize(textSizeRes);
-        lineWidth = res.getDimensionPixelSize(R.dimen.line_width);
-        topGap = res.getDimensionPixelSize(R.dimen.indent_pad_top);
-        bottomGap = res.getDimensionPixelSize(R.dimen.indent_pad_bottom_of_group);
-        lineInset = res.getDimensionPixelSize(R.dimen.line_inset);
-        textVerticalOffset = res.getDimensionPixelSize(R.dimen.line_vertical_offset);
-        int color = getColor(context, R.color.headerTextColor);
-        int shadowColor = getColor(context, R.color.shadow_black);
+        fetchColors(context);
 
-        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        linePaint.setStrokeWidth(lineWidth);
-        linePaint.setColor(color);
+        createPaints();
 
-        textPaint = new TextPaint();
-        fontMetrics = new Paint.FontMetrics();
-
-        textPaint.setTextSize(textSize);
-        textPaint.setColor(color);
-        textPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        textPaint.setAntiAlias(true);
-        textPaint.getFontMetrics(fontMetrics);
-        textPaint.setTextAlign(Paint.Align.CENTER);
+        makeMeasurements();
     }
 
+    private void fetchDimensions(Context context, @DimenRes int textSizeRes) {
+        final Resources res = context.getResources();
+        mTextSize = res.getDimensionPixelSize(textSizeRes);
+        mTopGap = res.getDimensionPixelSize(R.dimen.indent_pad_top);
+        mBottomGap = res.getDimensionPixelSize(R.dimen.indent_pad_bottom_of_group);
+        mTextVerticalOffset = res.getDimensionPixelSize(R.dimen.line_vertical_offset);
+    }
+
+    private void fetchColors(Context context) {
+        mBackdropColor = getColor(context, R.color.darker_background);
+        mTextColor = getColor(context, R.color.headerTextColor);
+    }
+
+    private void createPaints() {
+        mFontMetrics = new Paint.FontMetrics();
+        mTextPaint = new TextPaint();
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.getFontMetrics(mFontMetrics);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+
+        mBackdropPaint = new Paint();
+        mBackdropPaint.setColor(mBackdropColor);
+    }
+
+    private void makeMeasurements() {
+        mBackdropHeight = mTextPaint.getTextSize() + mFontMetrics.descent;
+    }
+
+    //endregion -- end --
+
     @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
-                               RecyclerView.State state) {
+    public void getItemOffsets(Rect outRect, View view,
+                               RecyclerView parent, RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
 
-        int pos = parent.getChildAdapterPosition(view);
-        long groupId = callback.getGroupId(pos);
-        if (groupId < 0) return;
-
-        if (isLastInGroup(pos)) {
-            outRect.bottom = bottomGap;
-        }
-
-        if (pos == 0 || isFirstInGroup(pos)) {
-            outRect.top = topGap;
-        }
-        else {
-            outRect.top = 0;
+        int childPosition = parent.getChildAdapterPosition(view);
+        if (mCallback.getGroupId(childPosition) >= 0) {
+            outRect.bottom = isLastInGroup(childPosition) ? mBottomGap : 0;
+            outRect.top = isFirstInGroup(childPosition) ? mTopGap : 0;
         }
     }
 
@@ -88,77 +107,48 @@ public class GroupDecoration extends RecyclerView.ItemDecoration {
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDraw(c, parent, state);
 
-        final int totalItemCount = state.getItemCount();
+        final int finalIndex = state.getItemCount() - 1;
         final int childCount = parent.getChildCount();
         final float width = parent.getWidth();
-        final float left = width / 2;
-
-        final float lineHeight = textPaint.getTextSize() + fontMetrics.descent;
+        final float textX = width / 2;
 
         long prevGroupId, groupId = -1;
         for (int i = 0; i < childCount; i++) {
             final View view = parent.getChildAt(i);
-            final int position = parent.getChildAdapterPosition(view);
+            final int childPosition = parent.getChildAdapterPosition(view);
 
+            // If this is the same group then skip
             prevGroupId = groupId;
-            groupId = callback.getGroupId(position);
-            if (groupId < 0 || groupId == prevGroupId) continue;
+            groupId = mCallback.getGroupId(childPosition);
+            boolean isTheSameGroup = groupId == prevGroupId;
+            if (groupId < 0 || isTheSameGroup) continue;
 
-            final String textLine = callback.getGroupFirstLine(position);
+            // If there isn't any text to draw then skip
+            final String textLine = mCallback.getGroupFirstLine(childPosition);
             if (textLine.isEmpty()) continue;
 
-            final int viewBottom = view.getBottom() + view.getPaddingBottom();
-            float textY = view.getTop() + view.getPaddingTop() - textVerticalOffset;
-            if (position + 1 < totalItemCount) {
-                long nextGroupId = callback.getGroupId(position + 1);
-                if (nextGroupId != groupId && viewBottom < textY + lineHeight) {
-                    textY = viewBottom - lineHeight;
-                }
-            }
+            float textY = view.getTop() + view.getPaddingTop() - mTextVerticalOffset;
 
-            Rect textRect = new Rect();
-            textPaint.getTextBounds(textLine, 0, textLine.length() - 1, textRect);
+            // Draw the backdrop
+            c.drawRect(0, textY - mBackdropHeight, width, textY + (mBackdropHeight / 3), mBackdropPaint);
 
-            final float lineY = view.getTop() + view.getPaddingTop() - textVerticalOffset - textRect.height()/2.0f + lineWidth / 4.0f;
-
-            float lineX1 = width / 2 - textRect.width()/2.0f - lineInset;// width - lineInset;
-            float lineX2 = width / 2 + textRect.width()/2.0f + lineInset;// width - lineInset;
-
-            int color = ContextCompat.getColor(mContext, R.color.darker_background);
-            Paint paint = new Paint();
-            paint.setColor(color);
-            c.drawRect(0,textY - lineHeight, width, textY + (lineHeight / 3), paint);
-//            c.drawRect(0, lineY, width, lineHeight, paint);
-
-//            c.drawLine(lineInset, lineY, lineX1, lineY, linePaint);
-//            c.drawLine(lineX2, lineY, width - lineInset, lineY, linePaint);
-
-            c.drawText(textLine, left, textY, textPaint);
+            // Draw the section text
+            c.drawText(textLine, textX, textY, mTextPaint);
         }
-
     }
 
     private boolean isFirstInGroup(int position) {
-        if (position == 0) {
-            return true;
-        }
+        if (position == 0) return true;
         else {
-            long prevGroupId = callback.getGroupId(position - 1);
-            long groupId = callback.getGroupId(position);
+            long prevGroupId = mCallback.getGroupId(position - 1);
+            long groupId = mCallback.getGroupId(position);
             return prevGroupId != groupId;
         }
     }
 
     private boolean isLastInGroup(int position) {
-        long nextGroupId = callback.getGroupId(position + 1);
-        long groupId = callback.getGroupId(position);
+        long nextGroupId = mCallback.getGroupId(position + 1);
+        long groupId = mCallback.getGroupId(position);
         return nextGroupId != groupId;
-    }
-
-    public interface Callback {
-        long getGroupId(int position);
-
-        @NonNull
-        String getGroupFirstLine(int position);
     }
 }
